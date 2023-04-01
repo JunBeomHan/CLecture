ae5735ade832b3d9c22e322c49244fcb1c4defb2
JunBumHan <2hanjunbum6@gmail.com>
2023-04-01 14:13:58 +0900

    01_01_exmaple.c

---
+++ Index/01 자료형과 상수와 변수/01_01_example.c
@@ -0,0 +1,15 @@
+// 01_01_example.c
+#include <stdio.h>
+
+int main() {
+	char myChar;
+	short myShort;
+	int myInt;
+	long myLong;
+	long long myLongLong;
+	float myFloat;
+	double myDouble;
+	long double myLongDouble;
+
+    return 0;
+}
