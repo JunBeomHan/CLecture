b7566c06403d48a341a88d8cdf00dfb35ab51f8e
JunBumHan <2hanjunbum6@gmail.com>
2023-04-01 14:13:44 +0900

    00_01_example.c

---
+++ Index/01 자료형과 상수와 변수/01_00_example.c
@@ -0,0 +1,13 @@
+// example 01_00_example.c
+#include <stdio.h>
+
+int main() {
+	// 먼저 정수형은 char, short, int, long, long long 으로 나뉩니다.
+	char val;
+	short val;
+	int val;
+	long val;
+	long long val;
+
+    return 0;
+}
