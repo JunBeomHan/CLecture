6531152fc886b872dda90987ea225593198f37f2
JunBumHan <2hanjunbum6@gmail.com>
2023-04-01 15:19:43 +0900

    00_03_example.c

---
+++ Index/02 표준 입출력 함수/00_03_example.c
@@ -0,0 +1,10 @@
+// example 02_03_example.c
+#include <stdio.h>
+
+int main() {
+	int height;
+	int weight;
+	scanf("%d %d", &hieght, &weight);
+	printf("키는 %d 입니다. 몸무게는 %d 입니다.", height, weight);
+	return 0;
+}
